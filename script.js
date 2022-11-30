74fe7130f1a056d0e0573fe002edb84a36c47da6
Hugo Gomes <hugorgomes10@gmail.com>
2022-11-30 00:40:04 -0300

    Function created to check if the square is full

--- script.js
+++ script.js
@@ -84,5 +84,13 @@ function checkWinnerFor(playerTurn) {
   return false;
 }
 
+function isFull() {
+  for (let i in square) {
+    if (square[i] === '') {
+      return false;
+    }
+  }
+  return true;
+}
 
 
