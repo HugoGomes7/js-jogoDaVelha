4daa0055448d206b0c7cf72af19fcfebe3b46a85
Hugo Gomes <hugorgomes10@gmail.com>
2022-11-30 00:15:37 -0300

    Created itemClick function and EventListener

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let square = {
-  a1: 'o', a2: 'o', a3: 'o',
+  a1: '', a2: '', a3: '',
   b1: '', b2: '', b3: '',
   c1: '', c2: '', c3: ''
 };
@@ -11,6 +11,33 @@ let playing = false;
 reset();
 
 document.querySelector('.reset').addEventListener('click', reset);
+document.querySelectorAll('.item').forEach(item => {
+  item.addEventListener('click', itemClick);
+});
+
+function itemClick(event) {
+  let item = event.target.getAttribute('data-item');
+  if (square[item] === '') {
+    square[item] = playerTurn;
+    renderSquare();
+  }
+}
+
+function reset() {
+  warning = '';
+
+  let random = Math.floor(Math.random() * 2);
+  playerTurn = (random === 0) ? 'x' : 'o';
+
+  for (let i in square) {
+    square[i] = ''
+  }
+
+  playing = true;
+
+  renderSquare();
+  renderInfo();
+}
 
 function renderSquare() {
   for (let i in square) {
@@ -24,18 +51,3 @@ function renderInfo() {
   document.querySelector('.result').innerHTML = warning;
 }
 
-function reset() {
-  warning = '';
-
-  let random = Math.floor(Math.random() * 2);
-  playerTurn = (random === 0) ? 'x' : 'o';
-
-  // for (let i in square) {
-  //   square[i] = ''
-  // }
-
-  playing = true;
-
-  renderSquare();
-  renderInfo();
-}
